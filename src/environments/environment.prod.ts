a2f11519cdd97129a7a544aabaac2ec3cd95ec42
fabian <fabianjra@hotmail.com>
2021-02-03 23:32:50 -0600

    FIX: Se agrega el firebaseConfig al ambiente de produccion. (provocaba error en deploy Netlify).

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCB49NEHTGTqEaB0Oh9tGycE9URWN6PqVs",
+    authDomain: "myspend-4753e.firebaseapp.com",
+    projectId: "myspend-4753e",
+    storageBucket: "myspend-4753e.appspot.com",
+    messagingSenderId: "374491335160",
+    appId: "1:374491335160:web:780e25313a3b56f86fee0d"
+  }
 };
