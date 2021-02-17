c105eb4b441a741015a69a6c0d0c66b703a231ab
fabian <fabianjra@hotmail.com>
2021-02-16 20:25:10 -0600

    Se quita el redireccionamiento de sitio al menu, para hacerlo en Home, en lugar de AppRoot.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,8 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from './services/auth.service';
-import { Utilities } from './shared/utilities';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
-  //Called first time before the ngOnInit()
-  constructor(private router: Router, private authService: AuthService) {
-    try {
-      let result = this.authService.getCurrentUser();
-
-      result.then((res) => {
-
-        if (res != null) {
-          this.router.navigate(['mainmenu']);
-        }
-      })
-        .catch((err) => {
-          let mensaje: string = Utilities.ObtenerMensajeErrorFB(err.code, err.message);
-          alert(mensaje); //F: Cambiar a Popup con diseño.
-        })
-
-    } catch (error) {
-      Utilities.LogErrorThrow((new Error).stack, error);
-    }
-  }
-
-}//FIN: Clase
+export class AppComponent { }
--- src/app/components/inicio/home/home.component.ts
+++ src/app/components/inicio/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 import { NavbarService } from 'src/app/services/navbar.service';
+import { Utilities } from 'src/app/shared/utilities';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +11,25 @@ import { NavbarService } from 'src/app/services/navbar.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private nav: NavbarService) { }
+  constructor(private nav: NavbarService, private authService: AuthService, private router: Router) {
+    try {
+      let result = this.authService.getCurrentUser();
+
+      result.then((res) => {
+
+        if (res != null) {
+          this.router.navigate(['mainmenu']);
+        }
+      })
+        .catch((err) => {
+          let mensaje: string = Utilities.ObtenerMensajeErrorFB(err.code, err.message);
+          alert(mensaje); //F: Cambiar a Popup con diseño.
+        })
+
+    } catch (error) {
+      Utilities.LogErrorThrow((new Error).stack, error);
+    }
+  }
 
   ngOnInit(): void {
     this.nav.show();
